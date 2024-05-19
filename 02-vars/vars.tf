8620014c853e5dc7b93130687e7479ce3ae8e5c7
naveenkumar <naveen6655@gmail.com>
2024-05-19 21:30:41 +0530

    List Variables output

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,22 @@ variable "x" {
 output "x" {
   value = var.x
 }
+#List Variable
+variable "l" {
+  default = [
+  10,
+  hello,
+  true
+  ]
+}
+
+output "l1" {
+  value = var.l
+
+}
+
+#accessing second value
+output "l2" {
+  value = var.l[1]
+
+}
