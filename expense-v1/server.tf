5e4380385aa14b360af58d82d96ae08096dfd45b
naveenkumar <naveen6655@gmail.com>
2024-05-19 20:43:49 +0530

    Terraform frontend backend mysql resource creation

--- expense-v1/server.tf
+++ expense-v1/server.tf
@@ -1,8 +1,24 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
-
   tags = {
     Name = "frontend"
+  }
+}
+
+resource "aws_instance" "backend"{
+  ami = ""
+  instance_type= "t3.micro"
+
+tags= {
+  Name = "backend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami = ""
+  instance_type = "t3.micro"
+  tags = {
+    Name = "mysql"
   }
 }
