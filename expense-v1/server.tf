9e65f1cd445d34fa350a9dc046616c0d2feb8dc0
naveenkumar <naveen6655@gmail.com>
2024-05-19 21:01:11 +0530

    Terraform adding security group

--- expense-v1/server.tf
+++ expense-v1/server.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09384db4af9c97a66"]
   tags = {
     Name = "frontend"
   }
@@ -9,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend"{
   ami = "ami-090252cbe067a9e58"
   instance_type= "t3.micro"
-
+  vpc_security_group_ids = ["sg-09384db4af9c97a66"]
 tags= {
   Name = "backend"
   }
@@ -18,6 +19,7 @@ tags= {
 resource "aws_instance" "mysql" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09384db4af9c97a66"]
   tags = {
     Name = "mysql"
   }
