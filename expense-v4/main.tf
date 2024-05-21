6e894df0cfbd1cf8570aaba4e1bc6e425903b7b0
naveenkumar <naveen6655@gmail.com>
2024-05-21 20:54:02 +0530

    Expense TF v4 Conditions

---
+++ expense-v4/main.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "ec2-nodes" {
+  for_each      = var.components
+  ami           = "ami-090252cbe067a9e58"
+  instance_type = try(each.value["instance_type"], null) == null ? "t3.small" : each.value["instance_type"]
+  vpc_security_group_ids = ["sg-09384db4af9c97a66"]
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "components" {
+  default = {
+    frontend = {}
+    backend  = {}
+    mysql = { instance_type = "t2.micro" }
+  }
+}
+
+#frontend = { instance_type = "t3.micro" }
+#In above input key is frontend value is  instance_type = "t3.micro"
+#since it is called by for_each, each prefix is needed like each.key and each.value.
