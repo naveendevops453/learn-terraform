1e864f543a4e92f2eac84753804d86f334deb7a0
naveenkumar <naveen6655@gmail.com>
2024-05-21 19:14:50 +0530

    tf filters/functions

--- 03-functions/main.tf
+++ 03-functions/main.tf
@@ -1,8 +1,9 @@
 variable "fruits" {}
 output "apple" {
-  value = "Apple Quantity - ${var.fruits["apple"]["quantity"]}, Price - ${var.fruits["apple"]["price"]}"
+  value = "Apple Quantity - ${lookup(var.fruits["apple"], "quantity", 0)}, Price - ${lookup(var.fruits["apple"], "price", 0.5)}"
 }
 
 output "banana" {
-  value = "Banana Quantity - ${var.fruits["banana"]["quantity"]}, Price - ${var.fruits["banana"]["price"]}"
+#  value = "Banana Quantity - ${var.fruits["banana"]["quantity"]}, Price - ${var.fruits["banana"]["price"]}"
+  value = "Banana Quantity - ${lookup(var.fruits["banana"], "quantity", 0)}, Price - ${lookup(var.fruits["banana"], "price", 0.5)}"
 }
