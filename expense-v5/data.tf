5e6cdf3759fe012aabe381384b3bf050057ef9c6
naveenkumar <naveen6655@gmail.com>
2024-05-26 22:36:18 +0530

    ami and security group using data source

--- expense-v5/data.tf
+++ expense-v5/data.tf
@@ -15,7 +15,6 @@ data "aws_security_group" "Allow-all" {
 # }
 
 data "aws_ami" "ami" {
-  executable_users = ["self"]
   most_recent      = true
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
